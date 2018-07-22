74dc448150606815c79c66df43a6e1359659de0f
Paco <paco@pacoorozco.info>
2018-07-22 10:49:02 +0200

    Add test to CI
    Signed-off-by: Paco <paco@pacoorozco.info>

---
+++ _scripts/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Script exits immediately if any command within it exits with a non-zero status
+set -o errexit
+# Script will catch the exit status of a previous command in a pipe.
+set -o pipefail
+# Script exits immediately if tries to use an undeclared variables.
+set -o nounset
+# Uncomment this to enable debug
+# set -o xtrace
+
+shellcheck bin/*.sh
+shellcheck install.sh
