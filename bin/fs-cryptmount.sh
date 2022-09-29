999bfa0b313be401429defa96a9d892892a384b1
pacoorozco <paco@pacoorozco.info>
2022-09-29 18:49:57 +0200

    Refactor of the fs-cryptmount.sh
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- bin/fs-cryptmount.sh
+++ bin/fs-cryptmount.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+
 ##########################################################################
 # Shellscript:  Mount an encFS filesystem
 # Author     :  Paco Orozco <paco@pacoorozco.info>
-# Requires   :
+# Requires   :  encfs, grep
 ##########################################################################
 # See CHANGELOG for changes.
 ##########################################################################
@@ -23,17 +24,22 @@
 ##########################################################################
 
 # Configuration variables
-Encrypted_FS_Path=~/Dropbox/ENCRYPTED
-Mounting_Point_Path=~/Private
-# How many time (minutes) will a mounted filesystem be idle before umount
-Idle_Timeout=10
+
+# Path to the encrypted filesystem
+encrypted_filesystem=~/Dropbox/Encrypted
+
+# Folder to mount the filesystem in plain
+plain_mount_point=~/Private
+
+# Enable automatic unmount of the filesystem after a period of inactivity (in minutes).
+idle_timeout=10
 
 ##########################################################################
 # DO NOT MODIFY BEYOND THIS LINE
 ##########################################################################
 # Program name and version
-Program_Name=$(basename "$0")
-Program_Version='0.0.5'
+program_name=$(basename "$0")
+program_version='0.0.6'
 
 # Script exits immediately if any command within it exits with a non-zero status
 set -o errexit
@@ -44,159 +50,132 @@ set -o nounset
 # Uncomment this to enable debug
 # set -o xtrace
 
-# Initialize variables in order to be used later
-Log_File=''
-# 0 - Quiet, 1 - Errors, 2 - Warnings, 3 - Normal, 4 - Verbose, 9 - Debug
-Verbosity_Level=3
-
-EncFS_Path=$(command -v encfs)
-fusermount_Path=$(command -v fusermount)
-
 ##########################################################################
 # Main function
 ##########################################################################
-function main () {
-  # Resetting OPTIND is necessary if getopts was used previously in the script.
-  OPTIND=1
-
-  # Process command line options
-  while getopts "hu" OPTION; do
-    case ${OPTION} in
-      h)
-        usage
-        safeExit 1
-        ;;
-      u)
-        fs_umount "${Mounting_Point_Path}"
-        safeExit
-        ;;
-      \?)
-        warning "Unknown option (ignored): -${OPTARG}"
-        ;;
-    esac
-  done
-  # Shift off the options and optional --.
-  shift "$((OPTIND - 1))"
+main() {
 
   check-requirements
 
-  EncFS_Path="${EncFS_Path} --idle=${Idle_Timeout}"
+  # declare global variables as readonly
+  readonly encrypted_filesystem
+  readonly plain_mount_point
+  readonly idle_timeout
 
-  # declare variables as readonly
-  readonly EncFS_Path
-  readonly Encrypted_FS_Path
-  readonly Mounting_Point_Path
+  while [ $# -gt 0 ]; do
 
-  if ! fs_mount "${Encrypted_FS_Path}" "${Mounting_Point_Path}"; then
-      die -e 1 "Can't mount ${Encrypted_FS_Path}..."
+    case $1 in
+    -h | -\? | --help)
+      show_help
+      safe_exit 0
+      ;;
+    -u | --unmount)
+      unmount_filesystem "${plain_mount_point}"
+      safe_exit $?
+      ;;
+    --) # End of all options.
+      shift
+      break
+      ;;
+    -?*)
+      echo "Unknown option (ignored): $1"
+      ;;
+    *) # Default case: No more options, so break out of the loop.
+      break
+      ;;
+    esac
+
+    shift
+
+  done
+
+  if is_filesystem_mounted "${plain_mount_point}"; then
+    echo "Filesystem already mounted at ${plain_mount_point}"
+    safe_exit 0
   fi
+
+  mount_filesystem "${encrypted_filesystem}" "${plain_mount_point}" &&
+    echo "Filesystem was mounted at ${plain_mount_point}"
 }
 
 ##########################################################################
 # Functions
 ##########################################################################
-# Do every cleanup task before exit.
-function safeExit () {
-  local _error_code=${1:-0}
-  exit "${_error_code}"
-}
-
-# Print a message, do format and treat verbose level
-declare -A LOG_LEVELS
-LOG_LEVELS=([error]=1 [warning]=2 [notice]=3 [info]=4 [debug]=9)
-
-function _alert () {
-  # TODO: This variables are reserved for future use
-  local color=""; local reset=""
-
-  # Print message to log file. Debug messages are not printed.
-  if [[ -n "${Log_File}" ]] && [[ "${1}" != "debug" ]]; then
-    echo -e "$(date +"%d-%m-%Y %X") $(printf "[%s]" "${1}") ${_message}" >> "${Log_File}"
-  fi
-
-  # Print to console depending of verbosity level
-  if [[ "${Verbosity_Level}" -ge "${LOG_LEVELS[${1}]}" ]]; then
-    echo -e "$(date +"%X") ${color}$(printf "[%s]" "${1}") ${_message}${reset}"
-  fi
-}
-
-# Print a message and exit
-function die () {
-  local _error_code=0
-  [[ "${1}" = "-e" ]] && shift; _error_code=${1}; shift
-  error "${*} Exiting."
-  safeExit "${_error_code}"
-}
-
-# Deal with severity level messages
-function error ()     { local _message="${*}"; _alert error >&2; }
-function warning ()   { local _message="${*}"; _alert warning >&2; }
-function notice ()    { local _message="${*}"; _alert notice; }
-function info ()      { local _message="${*}"; _alert info; }
-function debug ()     { local _message="${*}"; _alert debug; }
-function input()      { local _message="${*}"; _alert info; }
-
 # Check if requirements are satisfied
-function check-requirements () {
-  if [[ ! -x "${EncFS_Path}" ]]; then
-    die -e 127 "We require EncFS binary."
-  fi
-  if [[ ! -x "${fusermount_Path}" ]]; then
-    die -e 127 "We require fusermount binary."
-  fi
+function check-requirements() {
+  local -r required_binaries="encfs grep"
+
+  for binary in $required_binaries; do
+    if ! command -v "${binary}" >/dev/null 2>&1; then
+      die -e 127 "${binary} has NOT been found in PATH."
+    fi
+  done
 }
 
-function usage () {
+show_help() {
   # Variables for formatting
-  local U; U=$(tput smul)  # Underline
-  local RU; RU=$(tput rmul) # Remove underline
-  local B; B=$(tput bold)  # Bold
-  local N; N=$(tput sgr0)  # Normal
+  local -r U=$(tput smul)  # Underline
+  local -r RU=$(tput rmul) # Remove underline
+  local -r B=$(tput bold)  # Bold
+  local -r N=$(tput sgr0)  # Normal
 
   cat <<-EOF
     ${B}Usage:${N}
 
-    ${B}${Program_Name}${N} (v.${U}${Program_Version}${RU}) [options]...
+    ${B}${program_name}${N} (v.${U}${program_version}${RU}) [options]...
     ${B}Options:${N}
 
-    ${B}-h${N}  Display this help message
+    ${B}-h, --help${N}     Display this help message
 
-    ${B}-u${N}  Umount EncFS mounting point.
+    ${B}-u, --unmount${N}  Umount EncFS mounting point.
 
     ${B}Examples:${N}
 
-    \$ ${Program_Name}
+    \$ ${program_name}
     Minimal options. Will mount a specified EncFS into a specified mounting point.
 
-    \$ ${Program_Name} ${B}-u${N}
+    \$ ${program_name} ${B}--unmount${N}
     Will umount the EncFS.
 
 EOF
 }
 
 # Return if the encrypted FS is mounted
-function check_if_mounted() {
-  return "$(grep -c encfs /proc/mounts)"
+is_filesystem_mounted() {
+  grep --quiet "encfs" /proc/mounts
 }
 
 # Mount an encrypted filesystem into a mounting point
-function fs_mount () {
-  local Encrypted_FS="$1"
-  local Mounting_Point="$2"
+mount_filesystem() {
+  local -r filesystem=$1
+  local -r mount_point=$2
 
-  if check_if_mounted "${Mounting_Point}"; then
-    echo "Please suply credentials to mount ${Encrypted_FS}..."
-    ${EncFS_Path} "${Encrypted_FS}" "${Mounting_Point}" || return 1
-  fi
-  echo  "Succesfully mounted"
+  echo "Please supply the credentials to mount ${filesystem}..."
+
+  encfs --idle=${idle_timeout} "${filesystem}" "${mount_point}"
 }
 
-function fs_umount () {
-  local Mounting_Point="$1"
+# Umount the mounting point
+unmount_filesystem() {
+  local -r mount_point=$1
 
-  check_if_mounted "${Mounting_Point}" \
-    || ${fusermount_Path} -u "${Mounting_Point}" \
-    && echo "encfs... unmounted"
+  encfs -u "${mount_point}"
+}
+
+# Do every cleanup task before exit.
+safe_exit() {
+  local -r _error_code=${1:-0}
+  exit "${_error_code}"
+}
+
+# Print a message and exit
+die() {
+  local _error_code=0
+  [[ "${1}" = "-e" ]] && shift
+  _error_code=${1}
+  shift
+  echo "${*} Exiting."
+  safe_exit "${_error_code}"
 }
 
 ##########################################################################
